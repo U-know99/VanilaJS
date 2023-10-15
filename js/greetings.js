474366b83f4529bd440295276d31759ad1f5fb24
U-know99 <125248719+U-know99@users.noreply.github.com>
2023-10-16 07:17:53 +0900

    last
    last

--- js/greetings.js
+++ js/greetings.js
@@ -1,5 +1,6 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
@@ -18,6 +19,8 @@ function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginInput.classList.add(HIDDEN_CLASSNAME);
+  loginButton.classList.add(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
